27b4d6cee722d00269c2058b2c24f751542451a9
artemkorolev017 <139277146+artemkorolev017@users.noreply.github.com>
2023-07-14 00:00:53 +0300

    555

---
+++ qgis2web_2023_07_14-00_00_11_316592/data/_8.js
@@ -0,0 +1,1 @@
+var json__8 = {"type":"FeatureCollection","name":"_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"435345345"},"geometry":{"type":"Point","coordinates":[32.04860027474102,54.787113607298963]}}]}
