896b7d57982e00104017cf4d06140a896f1c3959
artemkorolev017 <139277146+artemkorolev017@users.noreply.github.com>
2023-07-15 06:30:49 +0300

    345345

---
+++ data/_9.js
@@ -0,0 +1,8 @@
+var json__9 = {
+"type": "FeatureCollection",
+"name": "_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Церковь Воскресения Христова", "descr": "Здесь воспитывалась мать Петра I - Наталья Нарышкина." }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 32.049484150791855, 54.784695744063441 ] ] } }
+]
+}
